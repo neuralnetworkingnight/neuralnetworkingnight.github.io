3bc559dc4dd49f979d7fd5832641ea6f61a0c9c6
Michael Graupner <michael.graupner@parisdescartes.fr>
2024-07-31 10:06:43 +0200

    Website update

--- publish.sh
+++ publish.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # Push the changes to aycogajiclab.github.io
 
-CHECKOUT="/tmp/caycogajiclab.github.io.git"
+#CHECKOUT="/tmp/caycogajiclab.github.io.git"
 
-rm -f -r $CHECKOUT
-git clone git@github.com:caycogajiclab/caycogajiclab.github.io.git $CHECKOUT
+#rm -f -r $CHECKOUT
+#git clone git@github.com:caycogajiclab/caycogajiclab.github.io.git $CHECKOUT
 
 BUILDPATH=$PWD
 
-cd $BUILDPATH/themes/hugo-academic-group
-git checkout main
-git pull
-cd $BUILDPATH
+#cd $BUILDPATH/themes/hugo-academic-group
+#git checkout main
+#git pull
+#cd $BUILDPATH
 
 rm -f -r $BUILDPATH/public/*
 
 hugo -t hugo-academic-group
 
-cd $CHECKOUT
-rm -f -r $CHECKOUT/*
-cp -r $BUILDPATH/public/* $CHECKOUT/
+#cd $CHECKOUT
+#rm -f -r $CHECKOUT/*
+#cp -r $BUILDPATH/public/* $CHECKOUT/
 
 git add -A
 git commit -m "Website update"
 git push origin main
 
-cd $BUILDPATH
-rm -f -r $CHECKOUT
+#cd $BUILDPATH
+#rm -f -r $CHECKOUT
 
 echo "All done."
